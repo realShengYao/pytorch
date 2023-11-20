f162e1d675ee5eec2e1c48750c4ab8b855da73e0
Edward Z. Yang <ezyang@meta.com>
2023-11-20 06:45:43 -0800

    [TESTING] Capture scalar/dynamic by default
    Signed-off-by: Edward Z. Yang <ezyangmeta.com>
    
    ghstack-source-id: 20d6cc7bf42a3af02b936f3210ae13c8681a4c82
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/110155

--- test/test_custom_ops.py
+++ test/test_custom_ops.py
@@ -1489,11 +1489,6 @@ def forward(self, x_1):
 
         f(torch.randn(10))
 
-        self.assertEqual(
-            dict(counters["graph_break"]),
-            {"dynamic shape operator: _torch_testing.numpy_nonzero.default": 1},
-        )
-
     # pre-existing problem: torch.compile(dynamic=True) will, by default,
     # graph break on data-dependent operations. Eventually we'll make it so
     # that it never graph breaks on data-dependent operations.
